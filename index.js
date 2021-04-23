c5d0363a57f0c14dbcd3f630e3dafa283531a2fd
lotiko <l.despres@netc.fr>
2021-04-23 10:19:49 +0200

    current state

--- index.js
+++ index.js
@@ -15,58 +15,77 @@ mongoose
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .catch((error) => {
-    console.error("Error connecting to the database", error);
-  })
-  .then((self) => {
+  .then(function (self) {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    Recipe.create(data[0])
-      .then((ret) => console.log(ret.title))
-      .catch((err) => console.log(err));
-  })
-  .then(() => {
-    Recipe.insertMany(data, { bypassDocumentValidation: false })
-      .then((docs) => {
+  .then(async function (ret) {
+    try {
+      await Recipe.create(data[0]);
+      console.log(doc.title);
+      await Recipe.insertMany(data, { bypassDocumentValidation: false }, function (err, docs) {
         docs.forEach((doc) => console.log(doc.title));
-      })
-      .catch((err) => console.log(err));
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+    // console.log(ret);
+    // return Recipe.create(data[0]);
+    // .then(function (ret) {
+    //   {
+    //     console.log(ret.title);
+    //     return ret;
+    //   }
+    // })
+    // .catch(function (err) {
+    //   console.log(err);
+    // });
   })
-  // .then(async (test) => {
-  //   // console.log(
-  //   //   // Run your code here, after you have insured that the connection was made
-  //   //   await Recipe.create(data[0]).then((ret) => {
-  //   //     console.log(ret.title);
-  //   //     return ret;
-  //   //   })
-  //   // );
-  //   // console.log(data.length);
-  //   // Toute les erreurs eventuel sont catcher par le catch en dessous
-  //   // let docsFromInsertMany =
-  //   console.log(
-  //     await Recipe.insertMany(data, {
-  //       bypassDocumentValidation: false,
+  // .then(function (ret) {
+  //   console.log(ret);
+  //   return Recipe.insertMany(data, { bypassDocumentValidation: false })
+  //     .then(function (docs) {
+  //       docs.forEach(function (doc) {
+  //         console.log(doc.title);
+  //       });
   //     })
-  //       .then((docs) => {
-  //         docs.forEach((doc) => console.log(doc.title));
-  //         return docs;
-  //       })
-  //       .catch((err) => console.log(err))
-  //   );
-  //   // docsFromInsertMany.forEach((doc) => console.log(doc.title));
-  //   let docUpdate = await Recipe.findOneAndUpdate(
-  //     { title: "Rigatoni alla Genovese" },
-  //     { duration: 100 },
-  //     { new: true }
-  //   );
-
-  //   console.log(
-  //     `Success document with _id: ${docUpdate._id} and title: ${docUpdate.title} have now duration set to ${docUpdate.duration}`
-  //   );
+  //     .catch(function (err) {
+  //       console.log(err);
+  //     });
   // })
   .catch((error) => {
-    console.error("OOps une erreur c'est produite => ", error);
+    console.error("Error connecting to the database", error);
   });
+// .then(async (test) => {
+//   // console.log(
+//   //   // Run your code here, after you have insured that the connection was made
+//   //   await Recipe.create(data[0]).then((ret) => {
+//   //     console.log(ret.title);
+//   //     return ret;
+//   //   })
+//   // );
+//   // console.log(data.length);
+//   // Toute les erreurs eventuel sont catcher par le catch en dessous
+//   // let docsFromInsertMany =
+//   console.log(
+//     await Recipe.insertMany(data, {
+//       bypassDocumentValidation: false,
+//     })
+//       .then((docs) => {
+//         docs.forEach((doc) => console.log(doc.title));
+//         return docs;
+//       })
+//       .catch((err) => console.log(err))
+//   );
+//   // docsFromInsertMany.forEach((doc) => console.log(doc.title));
+//   let docUpdate = await Recipe.findOneAndUpdate(
+//     { title: "Rigatoni alla Genovese" },
+//     { duration: 100 },
+//     { new: true }
+//   );
+
+//   console.log(
+//     `Success document with _id: ${docUpdate._id} and title: ${docUpdate.title} have now duration set to ${docUpdate.duration}`
+//   );
+// })
